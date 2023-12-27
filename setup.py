67d7e6c719bb3f5b076ef7b075c9351d2a10c95d
nomad-vagabond <keenon3d@gmail.com>
2023-12-28 00:45:53 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='splinecloud-scipy',
+    version='0.0.1',
+    author = "Vadym Pasko",
+    author_email = "vadym@splinecloud.com",
+    description = "A SplineCloud client based on SciPy.",
+    license = "MIT",
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'scipy',
+        'requests',
+    ],
+)
